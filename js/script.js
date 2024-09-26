de4e8a0b4cf2dc1c87a6d8eaff2faf0f9b41e5ee
GiorgiBondo <g.bondarenko09.gmal.com@gmail.com>
2024-09-26 17:46:43 +0400

    pet

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+
+
+function myFunction() {
+    var popup = document.getElementById("form");
+    let display = form.style.display;
+    if(display == "block" )
+    {
+        form.style.display = "none";
+    }
+    else{
+        form.style.display = "block";
+    }
+     
+  }
+
+
+
+
+
+
+
+ 
